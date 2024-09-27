b95aa4ea45279820ce7c577be2653f31b7ba8082
ismaila <iscomaila5623@gmail.com>
2024-09-27 10:54:03 +0000

    variables completed

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+// Variables
+let name = "ismaila"
+let age = 19
+let height = 180.0
+let workComplete = true
+
+console.log("Hello I am " + name + " and I am " + age + " years old. My height is " + height + " and work done is " + workComplete)
+
+console.log("The type of variables for the above variables are: name(" + typeof name + ") | age(" + typeof age + ") | height(" + typeof height + ") | workComplete(" + typeof workComplete + ")")
